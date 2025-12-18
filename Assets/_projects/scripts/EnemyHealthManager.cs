e9e5e9687b1f3b84039c0ced09d8beb58e7f6867
Owen Smith <562146@escg.ac.uk>
2025-12-18 09:54:14 +0000

    Snowball + Enemy Health

--- Assets/_projects/scripts/EnemyHealthManager.cs
+++ Assets/_projects/scripts/EnemyHealthManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,26 @@ using UnityEngine;
 public class EnemyHealthManager : MonoBehaviour
 {
     public int Health;
-    /*
+    Rigidbody RB;
 
-    public void TakeDamage(int Damage, Vector3 Origin, )
+    void Start()
     {
+        RB = (Rigidbody)gameObject.GetComponent("Rigidbody");
+    }
 
-    */
+    public void TakeDamage(int Damage, Vector3 Source, bool Knockback, float Force)
+    {
+        Health -= Damage;
+        if (Knockback)
+        {
+            RB.velocity = new Vector3(0, 0, 0);
+            Vector3 DirectionalForce = (transform.position - Source);
+            DirectionalForce = new Vector3(Math.Sign(DirectionalForce.x), 1, Math.Sign(DirectionalForce.z));
+            RB.velocity = DirectionalForce * Math.Abs(Force);
+        }
+        if (Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
